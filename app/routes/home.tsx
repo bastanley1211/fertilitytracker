f8c7a68165d12f8373e4f20a84bfe22d6ddcd53e
bastanley1211 <bastanley1211@protonmail.com>
2025-07-04 12:11:52 -0700

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import BBTTracker from "./bbt-tracker";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Fertility Tracker" },
+    { name: "description", content: "Welcome to Fertility Tracker!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <BBTTracker/>;
 }
